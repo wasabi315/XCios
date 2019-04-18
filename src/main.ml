67e159c7996927a84b8ebbc55084d4fe93933851
Arimichi Matsumura <12750219+popon-01@users.noreply.github.com>
2019-04-18 15:07:42 +0900

    Make executable

---
+++ src/main.ml
@@ -0,0 +1,18 @@
+let usage_msg =
+  "[Usage] emfrp-switch [file]..."
+
+let compile filename =
+  let ichan = open_in filename in
+  let lexbuf = Lexing.from_channel ichan in
+  (try
+    let prog = Parser.switchmodule Lexer.read lexbuf in
+    Syntax.pp_switchmodule Format.std_formatter prog
+   with
+   | Parser.Error ->
+      let pos = lexbuf.lex_curr_p in
+      Printf.printf "Syntax error : Line %d, Char %d."
+        pos.pos_lnum (pos.pos_cnum - pos.pos_bol + 1));
+  close_in ichan
+
+let () =
+  Arg.parse [] compile usage_msg
