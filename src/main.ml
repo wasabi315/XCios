d75311203d3487864f8de30c3049ae1d0ec99309
Arimichi Matsumura <12750219+popon-01@users.noreply.github.com>
2019-10-04 19:52:37 +0900

    Extend for multi-file

--- src/main.ml
+++ src/main.ml
@@ -10,8 +10,16 @@ let test_tsort ppf (progdata : Data.progdata) =
 *)
 
 exception ParseError of string
+exception FileError of string
 
 let parse filename =
+  let () = print_string filename; print_newline () in
+  let () =
+    if not (Sys.file_exists filename) then
+      let msg = Printf.sprintf "File %s is not found" filename in
+      raise (FileError msg)
+    else ()
+  in
   let ichan = open_in filename in
   let lexbuf = Lexing.from_channel ichan in
   try
@@ -20,43 +28,59 @@ let parse filename =
   with
   | Parser.Error ->
      let pos = lexbuf.lex_curr_p in
-     let mes =
-       Printf.sprintf "Syntax error in %s : Line %d, Char %d."
+     let msg =
+       Printf.sprintf "Syntax error in %s : Line %d, Char %d"
          filename pos.pos_lnum (pos.pos_cnum - pos.pos_bol + 1)
      in
      close_in_noerr ichan;
-     raise (ParseError mes)
+     raise (ParseError msg)
   | e ->
      close_in_noerr ichan;
      raise e
 
-(*
-let compile filename =
-  let ichan = open_in filename in
-  let lexbuf = Lexing.from_channel ichan in
-  (try
-     let prog = Parser.program Lexer.read lexbuf in
-     let pdata = Data.of_progdata prog in
-     let pdata = Typing.infer_progdata pdata in
-     Data.pp_progdata std_formatter pdata;
-     pp_print_newline std_formatter ();
-     test_tsort std_formatter pdata
-   with
-   | Parser.Error ->
-      let pos = lexbuf.lex_curr_p in
-      Printf.printf "Syntax error : Line %d, Char %d.\n"
-        pos.pos_lnum (pos.pos_cnum - pos.pos_bol + 1));
-  close_in ichan
- *)
+let get_filedata _data_map ast = ast
 
-let run filename =
-  let ast = parse filename in
-  pp_xfrp std_formatter ast
+type filestate = Visiting | Visited
+
+let gather_filedata entry_file =
+  let rec visit file (visit_state, data_map) =
+    match Idmap.find_opt file visit_state with
+    | None ->
+       let ast = parse file in
+       let use_files = List.map (fun f -> f ^ ".xfrp") ast.xfrp_use in
+       let (visit_state, data_map) =
+         List.fold_right visit use_files (visit_state, data_map)
+       in
+       let data = get_filedata data_map ast in
+       let data_map =  Idmap.add file data data_map in
+       let visit_state = Idmap.add file Visited visit_state in
+       (visit_state, data_map)
+    | Some(Visiting) -> raise (FileError "Detect cyclic file dependency")
+    | Some(Visited) -> (visit_state, data_map)
+  in
+  let (_, data_map) = visit entry_file (Idmap.empty, Idmap.empty)  in
+  data_map
+
+let codegen data_map =
+  Idmap.iter (fun id ast ->
+      print_string id;
+      print_newline ();
+      pp_xfrp std_formatter ast;
+      print_newline ();
+    ) data_map
+
+let compile file =
+  try
+    gather_filedata file |> codegen
+  with
+  | ParseError msg | FileError msg
+    -> printf "Compile Error : %s" msg;
+       print_newline ()
 
 let usage_msg =
   "[Usage] emfrp-switch [file]..."
 
 let () =
-  Arg.parse [] run usage_msg
+  Arg.parse [] compile usage_msg
 
 
